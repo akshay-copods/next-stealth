8c0fddf6b7b84cb33d96dfb3b95ca3a08cbce68d
Akshay Kamble <akshay.kamble@copods.co>
2023-02-14 14:25:36 +0530

    add landingpage banner

---
+++ src/components/LandingPageBanner/LandingPageBanner.tsx
@@ -0,0 +1,10 @@
+
+export const LandingPageBanner = () => {
+    return (
+        <div className="bg-geekblue-100 w-[34rem]">
+            <img src="https://picsum.photos/544/694" alt="" />
+        </div>
+    )
+}
+
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import { Header } from '@/components'
+import { LandingPageBanner } from '@/components/LandingPageBanner/LandingPageBanner'
 import { Inter } from '@next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <main className='flex flex-col h-screen'>
       <Header />
-    </>
+      <div className='flex h-full'>
+        <LandingPageBanner />
+      </div>
+    </main>
   )
 }
